ec2f5a5bb9b448c3221095f57efc70bfe834fde0
TINY PROGRAMMER <saqlainhassan1204@gmail.com>
2026-02-23 10:34:38 +0500

    Explore Event loop

---
+++ practice18.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+console.log("world");
+
+
+
+function Display(){
+    console.log("Ïam Saqlain");
+}
+
+console.log("How are You");
+Display(),3000;
---
+++ practice19.js
@@ -0,0 +1,13 @@
+console.log("start");
+
+setTimeout(() =>{
+console.log("First timeout");
+
+},0)
+
+
+setTimeout(() =>{
+    console.log("seccond TimeOut",0);
+});
+console.log("Second log");
+console.log("Finally Ended");
