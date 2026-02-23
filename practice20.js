04f9c51027d92b5eb0312358d9032e9fff006785
TINY PROGRAMMER <saqlainhassan1204@gmail.com>
2026-02-23 11:09:50 +0500

    Prime number

---
+++ practice20.js
@@ -0,0 +1,17 @@
+
+function Isprime(num){
+    if (num<1){
+        return false;
+    }
+for(let i=2;i<num;i++){
+    if(num % i === 0){
+return false;
+    }
+
+    
+}
+ return true;
+
+}
+
+console.log(Isprime(7));
