2c493c9008e6ac93f5c469a3c3a947d0aa2dd83e
TINY PROGRAMMER <saqlainhassan1204@gmail.com>
2026-02-25 12:20:35 +0500

    Used Promise

---
+++ practice21.js
@@ -0,0 +1,28 @@
+let firstpromise=new Promise((resolve,reject) =>{
+
+
+ setTimeout(function say(){
+    console.log("Iam Fronted Devoloper");
+},3000);
+
+   resolve();
+});
+
+
+
+let secondPromise=new Promise((resolve,reject) => {
+    let success=true;
+    if (success){
+        resolve("Fullfilled");
+    }
+    else{
+         reject("Erorr");
+    }
+    
+})
+secondPromise.then(message =>{
+console.log("Fullfiled ka mesage"+ message);
+});
+secondPromise.catch((message)){
+    console,log("Rejected ka mesage" +  message)
+};
